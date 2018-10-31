feed04cc5c26f766519ea48fca2223f73c272f4c
Jack Valinsky <jack.valinsky@gmail.com>
2018-10-30 20:57:18 -0400

    added navigation and beginnings of sign in

--- App.js
+++ App.js
@@ -1,22 +1,138 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import SafeMap from './components/SafeMap.js';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ActivityIndicator,
+  AsyncStorage,
+  StatusBar
+} from "react-native";
+import {
+  createSwitchNavigator,
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import SafeMap from "./components/SafeMap.js";
 
-export default class App extends React.Component {
-	render() {
-		return (
-			<SafeMap />
-        );
-	}
+class HomeScreen extends React.Component {
+  static navigationOptions = { title: "Home" };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <SafeMap />
+      </View>
+    );
+  }
 }
 
-    /*
-const styles = StyleSheet.create({
-	container: { flex: 1,
-    	backgroundColor: '#fff',
-    	alignItems: 'center',
-    	justifyContent: 'center',
-    },
-});
+class SettingsScreen extends React.Component {
+  static navigationOptions = { title: "Settings" };
 
-*/
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate("Auth");
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Settings Screen</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate("Home")}
+        />
+        <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
+      </View>
+    );
+  }
+}
+
+class AuthLoadingScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this._bootstrapAsync();
+  }
+
+  _bootstrapAsync = async () => {
+    const userToken = await AsyncStorage.getItem("userToken");
+
+    this.props.navigation.navigate(userToken ? "App" : "Auth");
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
+      </View>
+    );
+  }
+}
+
+class SignInScreen extends React.Component {
+  static navigationOptions = {
+    title: "Please sign in"
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Button title="Sign in!" onPress={this._signInAsync} />
+      </View>
+    );
+  }
+
+  _signInAsync = async () => {
+    await AsyncStorage.setItem("userToken", "abc");
+    this.props.navigation.navigate("App");
+  };
+}
+
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+const AppTab = createBottomTabNavigator(
+  { Home: HomeScreen, Settings: SettingsScreen },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "Home") {
+          iconName = `ios-map${focused ? "" : "-outline"}`;
+        } else if (routeName === "Settings") {
+          iconName = `ios-options${focused ? "" : "-outline"}`;
+        }
+        return (
+          <Ionicons
+            name={iconName}
+            size={horizontal ? 20 : 25}
+            color={tintColor}
+          />
+        );
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray"
+    }
+  }
+);
+
+const RootStack = createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppTab,
+    Auth: AuthStack
+  },
+  {
+    initialRouteName: "AuthLoading"
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
--- components/SafeMap.js
+++ components/SafeMap.js
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
-import { StyleSheet, Alert, View, TextInput } from 'react-native';
-import { Location, Permissions, MapView, SQLite, FileSystem, Asset } from 'expo';
-import { Callout } from 'react-native-maps';
+import React, { Component } from "react";
+import { StyleSheet, Alert, View, TextInput } from "react-native";
+import {
+  Location,
+  Permissions,
+  MapView,
+  SQLite,
+  FileSystem,
+  Asset
+} from "expo";
+import { Callout } from "react-native-maps";
 
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = 0.0421;
 const SPACE = 0.01;
 
 const styles = StyleSheet.create({
-	map: {
-		...StyleSheet.absoluteFillObject,
-    },
-    container: {
-            ...StyleSheet.absoluteFillObject,
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-          },
-    calloutView: {
-          flexDirection: "row",
-          backgroundColor: "rgba(255, 255, 255, 0.9)",
-          borderRadius: 10,
-          width: "40%",
-          marginLeft: "30%",
-          marginRight: "30%",
-          marginTop: 20
-    },
-    calloutSearch: {
-          borderColor: "transparent",
-          marginLeft: 10,
-          width: "90%",
-          marginRight: 10,
-          height: 40,
-          borderWidth: 0.0  
-        }
+  map: {
+    ...StyleSheet.absoluteFillObject
+  },
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  calloutView: {
+    flexDirection: "row",
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    borderRadius: 10,
+    width: "40%",
+    marginLeft: "30%",
+    marginRight: "30%",
+    marginTop: 20
+  },
+  calloutSearch: {
+    borderColor: "transparent",
+    marginLeft: 10,
+    width: "90%",
+    marginRight: 10,
+    height: 40,
+    borderWidth: 0.0
+  }
 });
 
 //const pins =
@@ -48,186 +55,194 @@ function openCB() {
   console.log("Database OPENED");
 }
 
-
 var DB = null; //= SQLite.openDatabase("file://Rnnr.db.sqlite", "1.0", "Rnnr Database", 200000, openCB, errorCB);
 
 //function query_db() {
 
 //}
 
-
-
-
 class SafeMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 43.128333,
+        longitude: -77.628333,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      },
+      markers: [],
+      errorMessage: null,
+      watchId: null
+    };
+    //FileSystem.documentDirectory
+    this._loadDBAsync();
+    console.log(FileSystem.documentDirectory);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            region: {
-                latitude: 43.128333, 
-                longitude: -77.628333,
-                latitudeDelta: LATITUDE_DELTA, 
-                longitudeDelta: LONGITUDE_DELTA,
-            },
-            markers: [],
-            errorMessage: null,
-            watchId: null,
-        }
-        //FileSystem.documentDirectory
-        this._loadDBAsync();
-        console.log(FileSystem.documentDirectory);
-    }
+  componentDidMount() {
+    this._getLocationAsync();
+  }
 
-    componentDidMount() {
-        this._getLocationAsync(); 
-    }
+  _loadDBAsync = async () => {
+    await FileSystem.downloadAsync(
+      Asset.fromModule(require("../assets/db/Rnnr.db.sqlite")).uri,
+      `${FileSystem.documentDirectory}Rnnr.db.sqlite`
+    );
 
-    _loadDBAsync = async () => {
-        await FileSystem.downloadAsync(
-            Asset.fromModule(require('../assets/db/Rnnr.db.sqlite')).uri,
-		    `${FileSystem.documentDirectory}Rnnr.db.sqlite`
-        );
+    console.log(
+      "Finished downloading " + `${FileSystem.documentDirectory}Rnnr.db.sqlite`
+    );
+    DB = SQLite.openDatabase(
+      "Rnnr.db.sqlite",
+      "3.0",
+      "Rnnr Database",
+      200000,
+      openCB,
+      errorCB
+    );
+    await this._queryAsync();
+  };
 
-        console.log('Finished downloading ' + `${FileSystem.documentDirectory}Rnnr.db.sqlite`
-);
-        DB = SQLite.openDatabase('Rnnr.db.sqlite'
-, "3.0", "Rnnr Database", 200000, openCB, errorCB);
-        await this._queryAsync();
-    }
+  _queryAsync = async () => {
+    console.log("query is being called");
 
-    _queryAsync = async () => {
-        console.log("query is being called");
+    DB.transaction(
+      (tx, results) => {
+        tx.executeSql(
+          "SELECT lat, lon, offense FROM offenders  WHERE lat IS NOT NULL",
+          [],
+          (tx, results) => {
+            console.log("Query completed");
 
-DB.transaction((tx, results) => {
-    tx.executeSql("SELECT lat, lon FROM offenders  WHERE lat IS NOT NULL", [], (tx, results) => {
-      console.log("Query completed");
+            // Get rows with Web SQL Database spec compliance.
 
-      // Get rows with Web SQL Database spec compliance.
-
-      var len = results.rows.length;
-      var coordinates = [];
-      console.log("results #: " + results.length);
-      for (let i = 0; i < len; i++) {
-        let row = results.rows.item(i);
-          console.log(`Lat: ${row.lat}, Lon: ${row.lon}`);
-        if(row.lat !== null) {
-            coordinates.push({key: (i+1), pinColor: '#ff0000', coordinate: {latitude: row.lat, longitude: row.lon}});
+            var len = results.rows.length;
+            var coordinates = [];
+            console.log("results #: " + results.length);
+            for (let i = 0; i < len; i++) {
+              let row = results.rows.item(i);
+              console.log(`Lat: ${row.lat}, Lon: ${row.lon}`);
+              if (row.lat !== null) {
+                coordinates.push({
+                  key: i + 1,
+                  pinColor: "#ff0000",
+                  offense: row.offense,
+                  coordinate: { latitude: row.lat, longitude: row.lon }
+                });
+              }
             }
+            let user_loc = {
+              latitude: this.state.region.latitude,
+              longitude: this.state.region.longitude
+            };
 
-      }
-      let user_loc =  {
-          latitude: this.state.region.latitude,
-          longitude: this.state.region.longitude
-      }
+            coordinates.push({
+              key: 0,
+              pinColor: "#0000ff",
+              coordinate: user_loc
+            });
+            this.setState({ markers: coordinates });
 
-      coordinates.push({key: 0, pinColor: '#0000ff', coordinate: user_loc});
-      this.setState({markers: coordinates});
+            // Alternatively, you can use the non-standard raw method.
 
-      // Alternatively, you can use the non-standard raw method.
-
-      /*
+            /*
         let rows = results.rows.raw(); // shallow copy of rows Array
 
         rows.map(row => console.log(`Employee name: ${row.name}, Dept Name: ${row.deptName}`));
-      */
-    }
-, (err) => { console.log(err)});
-}, (err) => {console.log("error: " + err)}, () => {console.log("success")});
-} 
-
-   _updateLocation = async (location) => {
-       this.setState({
-         region: {
-           latitude: location.coords.latitude,
-           longitude: location.coords.longitude,
-           latitudeDelta: LATITUDE_DELTA, 
-           longitudeDelta: LONGITUDE_DELTA
-         }
-       });
-       console.log(this.state.region);
-   }
-
-    _getLocationAsync = async () => {
-        let permission = await Permissions.askAsync(Permissions.LOCATION)
-            .catch((err) => { 
-                console.log(err); 
-            });
-        console.log(permission);
-        if (permission.status !== 'granted') {
-            this.setState({
-                errorMessage: 'Permission to access location was denied',
-            });
-        } else {
-            this.state.watchId = await Location.watchPositionAsync({
-                    enableHighAccuracy: true,
-                    timeInterval: 1000,
-                },
-                currentLocation => {
-                    this._updateLocation(currentLocation),
-                    console.log('updating!')
-                }
-            ).catch((err) => {
-                console.log(err);
-            });
-        }
-
-        if (this.state.errorMessage !== null) {
-            alert(this.state.errorMessage)
-        }
-
-        let location = await Location
-			.getCurrentPositionAsync({ enableHighAccuracy: true }, 
-				() => { 
-					this.setState({ 
-            		region: {
-                		latitude: location.coords.latitude,
-                		longitude: location.coords.longitude,
-                		latitudeDelta: LATITUDE_DELTA, 
-                		longitudeDelta: LONGITUDE_DELTA,
-            		},
-        		});
-			})
-            .catch((err) => {
-                console.log(err);
-            });
-    }
-    
-    render() {
-        return (
-			<View style={styles.container}>
-                <MapView style={styles.map} initialRegion={this.state.region}>
-                    {this.state.markers.map(marker => (
-            			<MapView.Marker
-                            coordinate={marker.coordinate}
-                            pinColor={marker.pinColor}
-                            key={marker.key}
-            			/>
-          			))}
-            </ MapView>
-  			<Callout>
-          		<View style={styles.calloutView} >
-                	<TextInput style={styles.calloutSearch}
-						placeholder={"Search"}
- 						onChangeText={(text) => console.log(text)}
-                     />
-				</View>
-            </Callout>
-			</View>
+        */
+          },
+          err => {
+            console.log(err);
+          }
         );
+      },
+      err => {
+        console.log("error: " + err);
+      },
+      () => {
+        console.log("success");
+      }
+    );
+  };
+
+  _updateLocation = async location => {
+    this.setState({
+      region: {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+    });
+    console.log(this.state.region);
+  };
+
+  _getLocationAsync = async () => {
+    let permission = await Permissions.askAsync(Permissions.LOCATION).catch(
+      err => {
+        console.log(err);
+      }
+    );
+    console.log(permission);
+    if (permission.status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied"
+      });
+    } else {
+      this.state.watchId = await Location.watchPositionAsync(
+        {
+          enableHighAccuracy: true,
+          timeInterval: 1000
+        },
+        currentLocation => {
+          this._updateLocation(currentLocation), console.log("updating!");
+        }
+      ).catch(err => {
+        console.log(err);
+      });
     }
-};
+
+    if (this.state.errorMessage !== null) {
+      alert(this.state.errorMessage);
+    }
+
+    let location = await Location.getCurrentPositionAsync(
+      { enableHighAccuracy: true },
+      () => {
+        this.setState({
+          region: {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA
+          }
+        });
+      }
+    ).catch(err => {
+      console.log(err);
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.map} initialRegion={this.state.region}>
+          {this.state.markers.map(marker => (
+            <MapView.Marker
+              coordinate={marker.coordinate}
+              pinColor={marker.pinColor}
+              key={marker.key}
+              description={marker.offense}
+            />
+          ))}
+        </MapView>
+      </View>
+    );
+  }
+}
 
 export default SafeMap;
 
-
 //select lat, lon from offenders where city == Bronx
 // success function callback, takes transaction and result set object
-//
-
-/*
-coordinate= {{
-                            latitude: this.state.region.latitude,
-                            longitude: this.state.region.longitude
-                        }}
-
-*/
